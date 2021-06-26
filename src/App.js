81cd04f69eb78536d12358ec5ec6cf0f78eb9ec4
bhavithmanapoty <76085215+bhavithmanapoty@users.noreply.github.com>
2021-06-26 21:47:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>Send a message to the Artist :)</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;

866b0de13058fc7725a3dc04dc651b092274f2d3
bhavithmanapoty <catchbhav2002@gmail.com>
2021-06-26 22:30:12 +0100

    Added image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ContactForm from './ContactForm';
+import Picture from './images/Pic2.jpg'
 
 const App = () => {
   return (
     <div className='App'>
       <div className='container'>
-        <h1 className='text-center'>Send a message to the Artist :)</h1>
+      <h1 className='text-center'>ENJOY YOUR TIME AT THE TUNNELS!</h1>
+        <img className='Image' src={Picture} alt={"Funny Image"}></img>
+        <h2 className='text-center'>Send a message to the Artist :)</h2>
         <ContactForm />
       </div>
     </div>

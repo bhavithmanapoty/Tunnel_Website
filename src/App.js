a88ce67529fca4bf290cbef81f40e5218c9030ba
bhavithmanapoty <catchbhav2002@gmail.com>
2021-06-27 23:29:52 +0100

    fixed image pos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ContactForm from './ContactForm';
-import Picture from './images/Pic2.jpg'
+import Picture from './images/Pic2 real.jpg'
+import Picture2 from './images/Pic1.jpg'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <div className='container'>
       <h1 className='text-center'>ENJOY YOUR TIME AT THE TUNNELS!</h1>
         <img className='Image' src={Picture} alt={"Funny Image"}></img>
+        <img className='Image' src={Picture2} alt={"Funny Image"}></img>
         <h2 className='text-center'>Send a message to the Artist :)</h2>
         <ContactForm />
       </div>

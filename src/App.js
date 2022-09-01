47b58a3ed2bac5319b5b1985ea81b1a3b324c201
Bhavith <catchbhav2002@gmail.com>
2022-09-01 13:42:39 -0400

    change to dil ki dosa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import ContactForm from './ContactForm';
-import Picture from './images/Pic2 real.jpg'
-import Picture2 from './images/Pic1.jpg'
+import ContactForm from "./ContactForm";
+import Picture from "./images/Pic2 real.jpg";
+import Picture2 from "./images/Pic1.jpg";
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='container'>
-      <h1 className='text-center'>ENJOY YOUR TIME AT THE TUNNELS!</h1>
-        <img className='Image' src={Picture} alt={"Funny Image"}></img>
-        <img className='Image' src={Picture2} alt={"Funny Image"}></img>
-        <h2 className='text-center'>Send a message to the Artist :)</h2>
+    <div className="App">
+      <div className="container">
+        <h2 className="text-center">WELCOME TO</h2>
+        <h1 className="text-center">DIL KI DOSA</h1>
+        <h3 className="text-center">
+          SEND US AN EMAIL IF YOU ARE INTERESTED IN JOINING :)
+        </h3>
         <ContactForm />
       </div>
     </div>
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,120 +1,150 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import emailjs from 'emailjs-com';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const ContactForm = () => {
   const { register, errors, handleSubmit, reset } = useForm();
   const [disabled, setDisabled] = useState(false);
 
   const toastifySuccess = () => {
-    toast('Form sent!', {
-      position: 'bottom-right',
+    toast("Form sent!", {
+      position: "bottom-right",
       autoClose: 5000,
       hideProgressBar: true,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: false,
-      className: 'submit-feedback success',
-      toastId: 'notifyToast'
+      className: "submit-feedback success",
+      toastId: "notifyToast",
     });
   };
 
-  function onSubmit(e)
-  {
+  function onSubmit(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_71h5x54', 'template_1', e.target, 'user_h5f5gH3nLwDYwBiCNdYRQ')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_cewb8lf",
+        "template_1",
+        e.target,
+        "user_h5f5gH3nLwDYwBiCNdYRQ"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
 
-      e.target.reset();
+    e.target.reset();
   }
 
   return (
-    <div className='ContactForm'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            <div className='contactForm'>
-              <form id='contact-form' onSubmit={onSubmit}>
+    <div className="ContactForm">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <div className="contactForm">
+              <form id="contact-form" onSubmit={onSubmit}>
                 {/* Row 1 of form */}
-                <div className='row formRow'>
-                  <div className='col-6'>
+                <div className="row formRow">
+                  <div className="col-6">
                     <input
-                      type='text'
-                      name='name'
+                      type="text"
+                      name="name"
                       ref={register({
-                        required: { value: true, message: 'Please enter your name' },
+                        required: {
+                          value: true,
+                          message: "Please enter your name",
+                        },
                         maxLength: {
                           value: 30,
-                          message: 'Please use 30 characters or less'
-                        }
+                          message: "Please use 30 characters or less",
+                        },
                       })}
-                      className='form-control formInput'
-                      placeholder='Name'
+                      className="form-control formInput"
+                      placeholder="Name"
                     ></input>
-                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                    {errors.name && (
+                      <span className="errorMessage">
+                        {errors.name.message}
+                      </span>
+                    )}
                   </div>
-                  <div className='col-6'>
+                  <div className="col-6">
                     <input
-                      type='email'
-                      name='email'
+                      type="email"
+                      name="email"
                       ref={register({
                         required: true,
-                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                        pattern:
+                          /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                       })}
-                      className='form-control formInput'
-                      placeholder='Email address'
+                      className="form-control formInput"
+                      placeholder="Email address"
                     ></input>
                     {errors.email && (
-                      <span className='errorMessage'>Please enter a valid email address</span>
+                      <span className="errorMessage">
+                        Please enter a valid email address
+                      </span>
                     )}
                   </div>
                 </div>
                 {/* Row 2 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
+                <div className="row formRow">
+                  <div className="col">
                     <input
-                      type='text'
-                      name='subject'
+                      type="text"
+                      name="subject"
                       ref={register({
-                        required: { value: true, message: 'Please enter a subject' },
+                        required: {
+                          value: true,
+                          message: "Please enter a subject",
+                        },
                         maxLength: {
                           value: 75,
-                          message: 'Subject cannot exceed 75 characters'
-                        }
+                          message: "Subject cannot exceed 75 characters",
+                        },
                       })}
-                      className='form-control formInput'
-                      placeholder='Subject'
+                      className="form-control formInput"
+                      placeholder="Subject"
                     ></input>
                     {errors.subject && (
-                      <span className='errorMessage'>{errors.subject.message}</span>
+                      <span className="errorMessage">
+                        {errors.subject.message}
+                      </span>
                     )}
                   </div>
                 </div>
                 {/* Row 3 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
+                <div className="row formRow">
+                  <div className="col">
                     <textarea
                       rows={3}
-                      name='message'
+                      name="message"
                       ref={register({
-                        required: true
+                        required: true,
                       })}
-                      className='form-control formInput'
-                      placeholder='Message'
+                      className="form-control formInput"
+                      placeholder="Message"
                     ></textarea>
-                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                    {errors.message && (
+                      <span className="errorMessage">
+                        Please enter a message
+                      </span>
+                    )}
                   </div>
                 </div>
 
-                <button className='submit-btn' disabled={disabled} type='submit'>
+                <button
+                  className="submit-btn"
+                  disabled={disabled}
+                  type="submit"
+                >
                   Submit
                 </button>
               </form>
